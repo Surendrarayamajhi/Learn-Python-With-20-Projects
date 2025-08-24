ddcd824aeca6bd1a11151962a6c1624f2161025a
Surendrarayamajhi <suren.rayamajhi1425@gmail.com>
2025-08-25 04:47:31 +0545

    this program for take two value from different variable in the form of int()

---
+++ variables/two.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the value of a:"))
+b = int(input("Enter the value of b:"))
+sum = a+b
+print("The value of Sum is =" ,sum)
+
+'''
+So here we are using int() to convert the string value which is by default generate by input
+'''
