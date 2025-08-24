c74f010d183a7a886fcbf406c1bac1613d968646
Surendrarayamajhi <suren.rayamajhi1425@gmail.com>
2025-08-25 05:33:44 +0545

    Arthematic Operation Program done

---
+++ arthematic/index.py
@@ -0,0 +1,24 @@
+num1 = int(input("Enter the fist number:"))
+num2 = int(input("Enter the second number:"))
+
+print("Here is addition of two number:\n")
+sum = num1+num2
+print("the two number sum is =" ,sum)
+
+print("Here is the Subtraction of two number:\n")
+
+sub = num1-num2
+
+print("Subtract from num1 to num2 is =" ,sub)
+
+print("Here is the division of two number:\n")
+
+div = num1/num2
+
+print("Divided from num1 to num2 is =" ,div)
+
+print("Here is the Multiplication of two number:\n")
+
+mul = num1*num2
+
+print("Multiply with num1 and num2 is =" ,mul)
